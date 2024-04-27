9c78d0df45eb4b39daf8ece71af84053cdff021f
devsafi <mdkawserferdoussafi@gmail.com>
2024-04-27 12:22:17 +0600

    created api for getting data by id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -39,6 +39,13 @@ async function run() {
             const cursor = artCollection.find();
             const result = await cursor.toArray();
             res.send(result);
+        })
+
+        app.get('/artItem/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const singleArt = await artCollection.findOne(query)
+            res.send(singleArt);
         })
 
 
